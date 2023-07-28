20050ddad343bd25df5ff35e8548f2a1abeeb126
yankaduchkevich <kadushkevichyanolegovich@gmail.com>
2023-07-28 19:12:15 +0300

    Added pokemon data and eslint

--- src/App.js
+++ src/App.js
@@ -1,29 +1,35 @@
+import React from 'react';
 import './App.css';
+import pokemon from './pokemon.json';
 
-function App() {
+const App = () => {
   return (
     <div style={{
-        margin: "auto",
-        width: 800,
-        paddingTop: "1rem"
+      margin: 'auto',
+      width: 800,
+      paddingTop: '1rem',
     }}>
       <h1 className="title">Pokemon search</h1>
-        <table width="100%">
-            <thead>
-              <tr>
-                  <th>Header</th>
-                  <th>Content</th>
+      <table width="100%">
+        <thead>
+          <tr>
+            <th>Header</th>
+            <th>Content</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            pokemon.map( (item) => (
+              <tr key={item.id}>
+                <td>{item.name.english}</td>
+                <td>{item.type.join(', ')}</td>
               </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Body</td>
-                    <td>Content</td>
-                </tr>
-            </tbody>
-        </table>
+            ))
+          }
+        </tbody>
+      </table>
     </div>
   );
-}
+};
 
 export default App;
